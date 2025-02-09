30720138398be799b89f5b5a86a3bfaa0e96d412
Maks <max.polak04@gmail.com>
2025-02-09 15:06:07 +0100

    added new methods and database.py file

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-import sqlite3
+from database import Database
 from sys import argv
 from os import getenv
 from dotenv import load_dotenv
 load_dotenv()
 
 
-class Database:
-    def __init__(self, database_name):
-        self.connection = sqlite3.connect(database_name)
-        self.cursor = self.connection.cursor()
-
-    def __del__(self):
-        self.connection.close()
-
-    def create_table(self, sql: str):
-        self.cursor.execute(sql)
-        self.connection.commit()
-
-
 if len(argv) == 2 and argv[1] == 'setup':
+    '''
+        Initialize DAtabase
+        Usage: python main.py setup
+    '''
     print('Creating table in datebase...')
     db = Database(getenv('DB_NAME'))
     db.create_table('''CREATE TABLE urls (
@@ -28,5 +19,21 @@ if len(argv) == 2 and argv[1] == 'setup':
                                 );''')
     
 if len(argv) == 4 and argv[1] == 'add':
+    '''
+        Adding new resource
+        python main.py add http://[website.com]
+    '''
     print('Adding new URL adres...')
+    category = argv[2]
+    url = argv[3]
     db = Database(getenv('DB_NAME'))
+    db.insert('urls', None, category, url)
+
+if len(argv) == 3 and argv[1] == 'list':
+    category = argv[2]
+    print(f'Link list from category {category}:')
+    db = Database(getenv('DB_NAME'))
+    links = db.fetch_all('urls', category=category)
+
+    for link in links:
+        print(link[2])
